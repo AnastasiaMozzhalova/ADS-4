3bad4ddc49ea50c26648a6062e4158f9b385974c
AnastasiaMozzhalova <mazhe.nasty@gmail.com>
2025-04-06 23:30:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -15,34 +15,51 @@ int countPairs1(int *arr, int len, int value) {
 
 int countPairs2(int *arr, int len, int value) {
   int count = 0;
-  int left = 0;
-  int right = len - 1;
-  while (left < right) {
-    int sum = arr[left] + arr[right];
-    if (sum == value) {
-      count++;
-      left++;
-      right--;
-      while (left < right && arr[left] == arr[left - 1]) left++;
-      while (left < right && arr[right] == arr[right + 1]) right--;
-    } else if (sum < value) {
-      left++;
-    } else {
-      right--;
+  int lb = 0;
+  int rb = len - 1;
+  for (int i = 0; i < rb; ++i) {
+    for (int j = rb-1; j > lb; --j) {
+      if (arr[j] > value) {
+        rb -= 1;
+      }
+      if (arr[i] + arr[j] == value) {
+        count += 1;
+      }
     }
+    lb += 1;
   }
   return count;
 }
 
+int binSer(int* arr, int left, int right, int target, bool find_first) {
+  int result = -1;
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+    if (arr[mid] == target) {
+      result = mid;
+      if (find_first) {
+        right = mid - 1;
+      } else {
+      left = mid + 1;
+      }
+      } else if (arr[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+  }
+  return result;
+}
+
 int countPairs3(int *arr, int len, int value) {
-  std::unordered_set<int> seen;
   int count = 0;
   for (int i = 0; i < len; ++i) {
-    int complement = value - arr[i];
-    if (seen.count(complement)) {
-      count++;
+    int element = value - arr[i];
+    int first = binSer(arr, i + 1, len - 1, element, true);
+    if (first != -1) {
+      int last = binSer(arr, first, len - 1, element, false);
+      count += last - first + 1;
     }
-  seen.insert(arr[i]);
   }
   return count;
 }

4c8aad61abce872c51fccc9fc792e2d50ca07e4a
AnastasiaMozzhalova <mazhe.nasty@gmail.com>
2025-04-21 13:59:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,61 +1,61 @@
 // Copyright 2021 NNTU-CS
-#include <iostream>
-#include <unordered_set>
-
-int countPairs1(const int *arr, int len, int value) {
+int countPairs1(int *arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i < len; ++i) {
-    for (int j = i + 1; j < len; ++j) {
+  for (int i = 0; i < len ; ++i) {
+    for (int j = i + 1; j < len ; ++j) {
       if (arr[i] + arr[j] == value) {
-        count++;
+        count += 1;
       }
     }
   }
-  return count;
+return count;
 }
-
-int countPairs2(const int *arr, int len, int value) {
+int countPairs2(int *arr, int len, int value) {
   int count = 0;
-  int lb = 0;
-  int rb = len - 1;
-  for (int i = 0; i < rb; ++i) {
-    for (int j = rb-1; j > lb; --j) {
+  int lboard = 0;
+  int rboard = len - 1;
+  for (int i = 0; i < rboard; ++i) {
+    for (int j = rboard-1; j > lboard; --j) {
       if (arr[j] > value) {
-        rb -= 1;
+        rboard -= 1;
       }
       if (arr[i] + arr[j] == value) {
         count += 1;
       }
     }
-    lb += 1;
+    lboard += 1;
   }
-  return count;
+return count;
+}
+int binSearc(int* arr, int left, int right, int target, bool find_first) {
+    int result = -1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (arr[mid] == target) {
+        result = mid;
+        if (find_first) {
+          right = mid - 1;
+        } else {
+          left = mid + 1;
+        }
+      } else if (arr[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    return result;
 }
 
-int bin(int *arr, int len, int target, int start) {
-  int left = start + 1;
-  int right = len - 1;
-  while (left <= right) {
-    int mid = left + (right - left) / 2;
-    if (arr[mid] == target) {
-      return mid;
-    } else if (arr[mid] < target) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
+int countPairs3(int *arr, int len, int value) {
+    int count = 0;
+    for (int i = 0; i < len; ++i) {
+      int complement = value - arr[i];
+      int first = binSearc(arr, i + 1, len - 1, complement, true);
+      if (first != -1) {
+        int last = binSearc(arr, first, len - 1, complement, false);
+        count += last - first + 1;
+      }
     }
-  }
-  return -1;
-}
-
-
-int countPairs3(const int *arr, int len, int value) {
- int count = 0;
-  for (int i = 0; i < len; ++i) {
-    int complement = value - arr[i];
-    if (bin(arr, len, complement, i) != -1) {
-      count++;
-    }
-  }
-  return count / 2;
+    return count;
 }

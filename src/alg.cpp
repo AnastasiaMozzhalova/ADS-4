ef7df96322d912952925366d34858de12a7e5c22
AnastasiaMozzhalova <mazhe.nasty@gmail.com>
2025-04-22 17:18:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,49 +1,84 @@
-
-int countPairs1(int* arr, int len, int value) {
-    int count = 0;
-    for (int i = 0; i < len - 1; ++i) {
-        if (i > 0 && arr[i] == arr[i - 1]) continue;
-        for (int j = i + 1; j < len; ++j) {
-            if (j > i + 1 && arr[j] == arr[j - 1]) continue;
-            if (arr[i] + arr[j] == value) {
-                ++count;
-            } else if (arr[i] + arr[j] > value) {
-                break;
-            }
-        }
+int countPairs1(int *arr, int size, int targetSum) {
+  int pairCount = 0;
+  for (int i = 0; i < size - 1; i++) {
+    for (int j = i + 1; j < size; j++) {
+      if (arr[i] + arr[j] == targetSum) {
+        pairCount++;
+      }
     }
-    return count;
+  }
+  return pairCount;
 }
-int countPairs2(int* arr, int len, int value) {
-    int count = 0;
-    int left = 0, right = len - 1;
-    while (left < right) {
-        int sum = arr[left] + arr[right];
-        if (sum == value) {
-            ++count;
-            int lval = arr[left], rval = arr[right];
-            while (left < right && arr[left] == lval) ++left;
-            while (left < right && arr[right] == rval) --right;
-        } else if (sum < value) {
-            ++left;
-        } else {
-            --right;
+int countPairs2(int *arr, int size, int targetSum) {
+  int pairCount = 0;
+  int highBound = size - 1;
+  while (highBound > 0 && arr[highBound] > targetSum) {
+    highBound--;
+  }
+  for (int lowBound = 0; lowBound < highBound; lowBound++) {
+    int currentValue = arr[lowBound];
+    int requiredValue = targetSum - currentValue;
+    int left = lowBound + 1;
+    int right = highBound;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (arr[mid] == requiredValue) {
+        pairCount++;
+        int temp = mid - 1;
+        while (temp >= left && arr[temp] == requiredValue) {
+          pairCount++;
+          temp--;
         }
+        temp = mid + 1;
+        while (temp <= right && arr[temp] == requiredValue) {
+          pairCount++;
+          temp++;
+        }
+        break;
+      } else if (arr[mid] < requiredValue) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
     }
-    return count;
+  }
+  return pairCount;
+}
+int findMatches(int *arr, int start, int end, int target) {
+  int firstOccurrence = -1;
+  int left = start;
+  int right = end;
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+    if (arr[mid] >= target) {
+      if (arr[mid] == target) firstOccurrence = mid;
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  if (firstOccurrence == -1) return 0;
+  int lastOccurrence = firstOccurrence;
+  left = firstOccurrence;
+  right = end;
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+    if (arr[mid] <= target) {
+      if (arr[mid] == target) lastOccurrence = mid;
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return lastOccurrence - firstOccurrence + 1;
 }
 
-int countPairs3(int* arr, int len, int value) {
-    int count = 0;
-    for (int i = 0; i < len - 1; ++i) {
-        if (i > 0 && arr[i] == arr[i - 1]) continue;
-        int target = value - arr[i];
-        const int* pos = std::lower_bound(arr + i + 1, arr + len, target);
-        if (pos != arr + len && *pos == target) {
-            if (pos == arr + i + 1 || *(pos - 1) != target) {
-                ++count;
-            }
-        }
-    }
-    return count;
+int countPairs3(int *arr, int size, int targetSum) {
+  int pairCount = 0;
+  for (int i = 0; i < size; i++) {
+    int complement = targetSum - arr[i];
+    if (complement < 0) continue;
+    pairCount += findMatches(arr, i + 1, size - 1, complement);
+  }
+  return pairCount;
 }

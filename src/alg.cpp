72238794da25caf38d2c3a32ab136f0b44c1b5a8
AnastasiaMozzhalova <mazhe.nasty@gmail.com>
2025-05-20 07:36:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,8 +1,8 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i < len; i++) {
-    for (int j = i + 1; j < len; j++) {
+  for (int i =0; i < len; ++i) {
+    for (int j = i + 1; j < len; ++j) {
       if (arr[i] + arr[j] == value) {
         count++;
       }
@@ -10,56 +10,54 @@ int countPairs1(int *arr, int len, int value) {
   }
   return count;
 }
-
 int countPairs2(int *arr, int len, int value) {
   int count = 0;
-  int left = 0, right = len - 1;
-  while (left < right) {
-    int sum = arr[left] + arr[right];
-    if (sum == value) {
-      int l = left;
-      int r = right;
-      while (l < r && arr[l] + arr[r] == value) {
-        ++count;
-        ++l;
+  int high = len - 1;
+  while (arr[high] > value) {
+    high = high - 1;
+  }
+  for (int i = 0; i < high; i++) {
+    for (int j = high; j > i; j--) {
+      if (arr[i] + arr[j] == value) {
+        count++;
       }
-      --right;
-    } else if (sum < value) {
-      ++left;
+    }
+  }
+  return count;
+}
+int cbinsearch(int *arr, int size, int value) {
+  int low = 0, count = 0, high = size - 1;
+  while (low <= high) {
+    int middle = (high + low) / 2;
+    if (arr[middle] < value) {
+      low = middle + 1;
+    } else if (arr[middle] > value) {
+      high = middle - 1;
     } else {
-      --right;
+      count++;
+      int now = middle - 1;
+      middle++;
+      while (arr[middle] == value) {
+        middle++;
+        count++;
+      }
+      while (arr[now] == value) {
+        now--;
+        count++;
+      }
+      break;
     }
   }
   return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len - 1; ++i) {
-    int target = value - arr[i];
-    int left = i + 1, right = len - 1;
-    while (left <= right) {
-      int mid = (left + right) / 2;
-      if (arr[mid] < target) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
-    }
-    int low = left;
-    left = i + 1;
-    right = len - 1;
-    while (left <= right) {
-      int mid = (left + right) / 2;
-      if (arr[mid] <= target) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
-    }
-    int high = right;
-    if (low <= high) {
-      count += (high - low + 1);
-    }
+  int i = 0, count = 0;
+  while (arr[i] <= (value / 2) -1) {
+    int num = value - arr[i];
+    count += cbinsearch(arr, len, num);
+    i++;
   }
+  int middle = cbinsearch(arr, len, value /2);
+  count += middle * (middle - 1) / 2;
   return count;
 }
